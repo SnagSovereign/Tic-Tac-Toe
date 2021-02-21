4a022b8b33df7dbb75886e81d3ec408617620bbe
SnagSovereign <joeysimon@outlook.com>
2021-02-21 20:19:42 +1100

    X/O spawn on click and snap to grid. Player turn switches.

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	[Header("Player Turn Symbols")]
+	[SerializeField] GameObject crossSymbol;
+	[SerializeField] GameObject noughtSymbol;
+
+	public static bool crossTurn = true;
+
+	public void SwitchTurn()
+    {
+		crossTurn = !crossTurn;
+		crossSymbol.SetActive(!crossSymbol.activeSelf);
+		noughtSymbol.SetActive(!noughtSymbol.activeSelf);
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour 
+{
+    GameManager GM;
+
+    private void Awake()
+    {
+        GM = FindObjectOfType<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        Spawn(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return SnapToGrid(worldPos);
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        return new Vector2(Mathf.RoundToInt(rawWorldPos.x), Mathf.RoundToInt(rawWorldPos.y));
+    }
+
+    private void Spawn(Vector2 gridPos)
+    {
+        string symbol;
+        if (GameManager.crossTurn) { symbol = "cross"; }
+        else { symbol = "nought"; }
+
+        Instantiate(Resources.Load<GameObject>(symbol), gridPos, transform.rotation);
+        GM.SwitchTurn();
+    }
+}
